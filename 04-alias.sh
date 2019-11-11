caf696e09857636f7449e213627045e871b051a2
Eungbean Lee <eungbean@yonsei.ac.kr>
2019-11-11 16:21:01 +0900

    docker-vscode

--- 04-alias.sh
+++ 04-alias.sh
@@ -7,5 +7,6 @@ alias gpu="watch -d -n 0.5 nvidia-smi"
 alias gpu2="gpustat -i"" >> ${ZDOTDIR:-$HOME}/.zshrc
 
 # after container launch
-echo "alias lab='docker exec -it lab /bin/bash'" >> ${ZDOTDIR:-$HOME}/.zshrc
+echo "alias lab='docker exec -it lab /usr/bin/zsh'" >> ${ZDOTDIR:-$HOME}/.zshrc
+echo "alias lab='docker exec -it code /usr/bin/zsh'" >> ${ZDOTDIR:-$HOME}/.zshrc
 source ~/.zshrc
