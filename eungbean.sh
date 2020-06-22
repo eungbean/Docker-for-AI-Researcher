cc5331a868f670d8d27e1d115177aaeb13bd8a88
Eungbean Lee <eungbean@yonsei.ac.kr>
2020-06-22 07:13:35 +0000

    apply gitignore

--- eungbean.sh
+++
@@ -1,5 +0,0 @@
-echo "export DATASET='/data/DATASET'" >> ${ZDOTDIR:-$HOME}/.zshrc
-echo "export DAVIS='/data/DATASET/DAVIS'" >> ${ZDOTDIR:-$HOME}/.zshrc
-echo "export DAVISimage='/data/DATASET/DAVIS/JPEGImages/480p'" >> ${ZDOTDIR:-$HOME}/.zshrc
-echo "export DAVISannot='/data/DATASET/DAVIS/Annotations/480p'" >> ${ZDOTDIR:-$HOME}/.zshrc
-source ~/.zshrc
